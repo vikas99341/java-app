353dd6ed9e657b6a0acfd2dd856f6ac6c01514e1
vikas99341 <vikasislove@gmail.com>
2021-12-16 03:12:19 +0000

    Secondt commit

--- src/main/java/com/vikas/app/App.java
+++ src/main/java/com/vikas/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World FROM java !" );
+        System.out.println( "Hello World FROM java and Learnig is fun !" );
     }
 }
