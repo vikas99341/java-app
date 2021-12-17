c26ed1ccce0247958de6e53e20787ee55819192f
vikas99341 <48900898+vikas99341@users.noreply.github.com>
2021-12-17 08:34:09 +0530

    update app.java

--- src/main/java/com/vikas/app/App.java
+++ src/main/java/com/vikas/app/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World from java and Learnig is easy !!!!" );
+        System.out.println( "Hello World from java and Learnig is easy !!!!!!" );
    }
 }
